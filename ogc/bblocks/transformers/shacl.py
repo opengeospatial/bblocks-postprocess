869ca65a190e49b779f63a93724fe3a7f74c6801
Alejandro Villar <avillar@ogc.org>
2023-10-30 11:39:06 +0100

    Disable rdflib warnings for transforms

--- ogc/bblocks/transformers/shacl.py
+++ ogc/bblocks/transformers/shacl.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 from typing import AnyStr
 
 from ogc.bblocks.util import TransformMetadata
@@ -19,9 +20,16 @@ target_mime_types = [
     'application/ld+json',
 ]
 
+rdflib_logger = logging.getLogger('rdflib')
+
 
 def transform(metadata: TransformMetadata) -> AnyStr:
-    data_graph = Graph().parse(data=metadata.input_data, format=metadata.source_mime_type)
-    shacl_graph = Graph().parse(data=metadata.transform_content)
-    pyshacl.validate(data_graph=data_graph, shacl_graph=shacl_graph, advanced=True, inplace=True)
+    rdflib_level = rdflib_logger.level
+    try:
+        rdflib_logger.setLevel(logging.ERROR)
+        data_graph = Graph().parse(data=metadata.input_data, format=metadata.source_mime_type)
+        shacl_graph = Graph().parse(data=metadata.transform_content)
+        pyshacl.validate(data_graph=data_graph, shacl_graph=shacl_graph, advanced=True, inplace=True)
+    finally:
+        rdflib_logger.setLevel(rdflib_level)
     return data_graph.serialize(format=metadata.target_mime_type)
