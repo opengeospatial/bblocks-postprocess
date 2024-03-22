74b32570d49c0a0787abb7fe1d8fef219967038c
Alejandro Villar <avillar@ogc.org>
2024-03-22 08:48:46 +0100

    Remove test file

--- ogc/bblocks/test.py
+++
@@ -1,7 +0,0 @@
-import jsonschema
-from ogc.na.util import load_yaml
-
-d = load_yaml(filename='/home/alx5000/work/Proyectos/ogc/3d-csdm-schema/_sources/csdm/test/example.json')
-schema = load_yaml(filename='/home/alx5000/work/Proyectos/ogc/3d-csdm-schema/build-local/annotated/csdm/test/schema'
-                            '.json')
-jsonschema.validate(d, schema)
