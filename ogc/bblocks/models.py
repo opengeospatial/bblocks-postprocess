637286ce2e767f9dc47dcb3e087d7ac254393bc1
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2026-09-11 17:29:31 -0400

    Add hasFormat relation for alternate-format sibling Building Blocks
    hasFormat represents dcterms:hasFormat/isFormatOf semantics: two Building
    Blocks that are substantially the same content in another format (e.g. a
    JSON vs an XML serialization of the same model), as opposed to isProfileOf's
    specialization/inheritance semantics.
    
    - bblock.schema.yaml: add hasFormat (array of bblock references), symmetric,
      intended to be declared on every sibling that should be linked
    - models.py: normalize bblocks:// prefixes on hasFormat like dependsOn/isProfileOf
    - register-context.yaml: map hasFormat to dct:hasFormat, and add the
      previously-missing seeAlso -> rdfs:seeAlso mapping (the property name itself
      had no JSON-LD context term, unlike its raw rdfs:seeAlso IRI counterpart)

--- ogc/bblocks/models.py
+++ ogc/bblocks/models.py
@@ -233,6 +233,9 @@ class BuildingBlock:
         if 'dependsOn' in self.metadata:
             self.metadata['dependsOn'] = [strip_bblocks_uri(d) for d in self.metadata['dependsOn']]
 
+        if 'hasFormat' in self.metadata:
+            self.metadata['hasFormat'] = [strip_bblocks_uri(d) for d in self.metadata['hasFormat']]
+
         ep = self.metadata.get('extensionPoints')
         if ep:
             if 'baseBuildingBlock' in ep:
@@ -692,6 +695,8 @@ class ImportedBuildingBlocks:
             bblock['register'] = self
             if 'dependsOn' in bblock:
                 bblock['dependsOn'] = [strip_bblocks_uri(d) for d in bblock['dependsOn']]
+            if 'hasFormat' in bblock:
+                bblock['hasFormat'] = [strip_bblocks_uri(d) for d in bblock['hasFormat']]
             if 'isProfileOf' in bblock:
                 v = bblock['isProfileOf']
                 bblock['isProfileOf'] = ([strip_bblocks_uri(i) for i in v]
